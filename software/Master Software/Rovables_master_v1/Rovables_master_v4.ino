6a4047848b7d4cb1fe741f5b68d1c019b1eee672
adementyev <artemd@mit.edu>
2018-02-05 15:56:44 -0500

    added nrf24 library

---
+++ software/Master Software/Rovables_master_v1/Rovables_master_v4.ino
@@ -0,0 +1,260 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+//Define pins
+#define  LED_BLUE     A3 
+#define  LED_RED      A4
+#define  CE           10  
+#define  CSN          9
+#define BUTTON_X      A9
+#define BUTTON_Y      A8
+#define  DEBUG_PIN    0
+#define MOTOR_OPCODE  0x02
+#define POTENTIOMETER A4 
+
+
+#define PACKET_SIZE     15
+#define BUFFER_SIZE     30
+#define NUMBER_OF_NODES 6
+
+//#define JOYSTICK CONTROL 
+#define COMPUTER CONTROL
+
+RF24 radio(CE,CSN); // Default SPI speed is 4Mbit, but should work up to 10MBbit
+
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pipes[] = {0x7878787878LL, 0xB3B4B5B6F1LL, 0xB3B4B5B6CDLL, 0xB3B4B5B6A3LL, 0xB3B4B5B60FLL, 0xB3B4B5B605LL };
+
+
+bool SERIAL_DEBUG  = true;
+uint8_t receiveData[PACKET_SIZE] = { };
+uint8_t sendData[PACKET_SIZE] = { };
+
+int state = 0;
+int speed_forward  = 0;  
+
+uint8_t robotUnderControl = 0;
+
+uint8_t serialBuffer[BUFFER_SIZE] = { }; //Initialize to zeros
+uint8_t computerReceiveBuffer[PACKET_SIZE][NUMBER_OF_NODES] = { };
+
+
+void setup(void)
+{
+
+  pinMode(LED_BLUE, OUTPUT); 
+ // pinMode(LED_RED, OUTPUT);
+  pinMode(DEBUG_PIN, OUTPUT);
+  pinMode(POTENTIOMETER, INPUT);
+
+  SerialUSB.begin(0);
+
+  radio.begin();
+
+  radio.setRetries(10,0); //retries, delay
+  radio.setPayloadSize(PACKET_SIZE);
+  radio.setDataRate(RF24_2MBPS);
+  radio.setChannel(60);
+
+  radio.openReadingPipe(0,pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.openReadingPipe(2,pipes[2]);
+  radio.openReadingPipe(3,pipes[3]);
+  radio.openReadingPipe(4,pipes[4]);
+  radio.openReadingPipe(5,pipes[5]);
+
+  radio.startListening();
+
+  digitalWrite(LED_RED, HIGH); 
+ 
+}
+
+void loop(void)
+{
+
+  //Under joystick control. 
+  getJoyStickData();
+  
+  //--------------CHECK FOR SERIAL PORT DATA---------------
+  byte pipeNum = 0; //variable to hold which reading pipe sent data
+  byte gotByte = 0; //used to store payload from transmit module
+  int numBytes =0;
+  while(Serial.available()) { 
+    serialBuffer[numBytes] = SerialUSB.read();
+    numBytes++;  
+  }
+  
+
+  if (serialBuffer[0] == '@') { 
+      int whereToSend = serialBuffer[1]*256; 
+      whereToSend = whereToSend + serialBuffer[2];
+      byte opCode = serialBuffer[3]; 
+      byte sizeofData = serialBuffer[4]; 
+      for (int i=1; i<sizeofData; i++) {
+        computerReceiveBuffer[i][whereToSend] = serialBuffer[i+4]; 
+      }//end for 
+      computerReceiveBuffer[0][whereToSend] = opCode;
+
+      for(int i=0; i<sizeof(serialBuffer); i++) serialBuffer[i] =0;
+
+      //for(int i=0; i<PACKET_SIZE; i++) { 
+      //  Serial.println(computerReceiveBuffer[i][whereToSend]);
+      //}
+      
+  }
+  
+  //------------CHECK THE RADIO----------------------------
+  while(radio.available(&pipeNum)){ //Check if received data
+     digitalWrite(DEBUG_PIN,HIGH);
+     radio.read( &receiveData, PACKET_SIZE ); //read one byte of data and store it in gotByte variable
+     //printReceiveData(pipeNum);
+     printReceiveDataFormated(pipeNum);
+     delay(5);
+
+      
+     //Comment out below to enable joystick control
+     #ifdef COMPUTER_CONTROL
+     for(int i=0; i<PACKET_SIZE; i++) { 
+      sendData[i] = computerReceiveBuffer[i][pipeNum];
+     }
+     #endif
+
+     if (sendToSlave(pipeNum,  PACKET_SIZE)) {
+        //Success sending   
+     }
+     else {
+        //failed sending
+     }
+     digitalWrite(DEBUG_PIN,LOW);
+  }//end while
+}//end loop
+
+bool sendToSlave(byte whichPipe, uint8_t packet_size) { 
+     bool worked;   
+     radio.stopListening(); 
+     radio.openWritingPipe(pipes[whichPipe]); 
+     if(!radio.write(&sendData,packet_size)) worked = false; 
+     else worked = true; 
+     radio.startListening(); 
+     return worked; 
+}//end sendToSlave
+
+void printReceiveData(byte whichPipe) { 
+   SerialUSB.print("S");
+   SerialUSB.print(",");
+   SerialUSB.print(whichPipe); 
+   SerialUSB.print(",");
+  for (int i=0; i<PACKET_SIZE; i++) {
+         SerialUSB.print(receiveData[i]); 
+         if(i<PACKET_SIZE-1){
+          SerialUSB.print(",");
+         }
+  }
+  SerialUSB.println(" ");
+}
+
+void printReceiveDataFormated(byte whichPipe) { 
+   SerialUSB.print("S");
+   SerialUSB.print(",");
+   SerialUSB.print(whichPipe); 
+   SerialUSB.print(",");
+   SerialUSB.print(receiveData[0]); 
+   SerialUSB.print(","); 
+   SerialUSB.print(receiveData[1]); 
+   SerialUSB.print(","); 
+   SerialUSB.print(receiveData[2]); 
+   SerialUSB.print(","); 
+
+   unsigned long d;
+    d =  (receiveData[6] << 24) | (receiveData[5] << 16) | (receiveData[4] << 8) | (receiveData[3]);
+   float member = *(float *)&d;
+   SerialUSB.print( member ); 
+   SerialUSB.print(","); 
+
+   d =  (receiveData[10] << 24) | (receiveData[9] << 16) | (receiveData[8] << 8) | (receiveData[7]);
+   member = *(float *)&d;
+   SerialUSB.print( member ); 
+
+   SerialUSB.print(","); 
+
+   d =  (receiveData[14] << 24) | (receiveData[13] << 16) | (receiveData[12] << 8) | (receiveData[11]);
+   member = *(float *)&d;
+   SerialUSB.println( member ); 
+
+}
+
+void getJoyStickData() { 
+  int forward_back = analogRead(BUTTON_X); 
+  int left_right = analogRead(BUTTON_Y); 
+  float speed_scalar = (float)analogRead(POTENTIOMETER)/594.0;
+  speed_forward  = 0;  
+  int intesityTurn = 0; 
+  if (forward_back>=556) { 
+    speed_forward = speed_scalar*((forward_back-556)/2);
+    
+  }
+  else if (forward_back<556) {
+     speed_forward = speed_scalar*((556-forward_back)/2)-25;
+     if(speed_forward <=0)  speed_forward = 0;
+  }
+
+  //debugButtons();
+
+  if (left_right >700) { 
+    //turn LEFT
+      sendData[0] = MOTOR_OPCODE;
+      sendData[1] = 120; 
+      sendData[2] = 120;
+      sendData[3] = 1;
+      sendData[4] = 1;
+      state = 1; 
+  }
+
+  else if (left_right < 300) { 
+    //turn RIGHT
+      sendData[0] = MOTOR_OPCODE;
+      sendData[1] = 130; 
+      sendData[2] = 130;
+      sendData[3] = 0;
+      sendData[4] = 0;
+      state =2;
+  }
+
+  else if (forward_back > 600) { 
+    //go FORWARD 
+      sendData[0] = MOTOR_OPCODE;
+      sendData[1] = speed_forward; 
+      sendData[2] = speed_forward; 
+      sendData[3] = 1;
+      sendData[4] = 0;
+      state = 3; 
+  }
+
+  else if (forward_back < 400) {
+    //go BACKWARDS
+      sendData[0] = MOTOR_OPCODE;
+      sendData[1] = speed_forward;  //turning left  60/60 original
+      sendData[2] = speed_forward;
+      sendData[3] = 0;
+      sendData[4] = 1;
+      state = 4;
+  }
+
+  else  { 
+    sendData[0] = MOTOR_OPCODE;
+    sendData[1] = 0; 
+    sendData[2] = 0;
+    state = 0;
+  }
+}
+
+void debugButtons() { 
+  SerialUSB.print(analogRead(BUTTON_X));
+  SerialUSB.print(",");
+  SerialUSB.print(analogRead(BUTTON_Y));
+  SerialUSB.print(","); 
+  SerialUSB.print(state);
+  SerialUSB.print(",");
+  SerialUSB.println(speed_forward); 
+}
